3635251614c9af4b9461154e3c12dd10f345543a
Huang ShengJun <1054880335@qq.com>
2020-07-31 11:33:29 +0800

    feat:备份；

--- DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/Modules/Bookmarks/Modules/5BinaryTreeAndGraph/Modules/PrePareBinaryTree.hpp
+++ DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/Modules/Bookmarks/Modules/5BinaryTreeAndGraph/Modules/PrePareBinaryTree.hpp
@@ -11,7 +11,16 @@
 
 #include <stdio.h>
 #include "TreeNode.h"
+/*
+预备知识：二叉树定义
+树是n（n>=0）个节点的有限集，且这些节点满足如下关系：
+（1）有且仅有一个节点没有父节点，该节点称为树的根。
+（2）除根外，其余的每个节点都有且仅有一个父节点。
+（3）树中的每一个节点都构成一个以它为根的树。
 
+二叉树在满足树的条件时，满足如下条件：
+每个节点最大有两个孩子（子树），这两个子树有左右之分，次序不可颠倒。
+*/
 class PrePareBinaryTree {
 public:
     void sec1demo1();
@@ -21,14 +30,22 @@ private:
 
 
 
-#endif /* PrePareBinaryTree_hpp */
 /*
- 预备知识：二叉树定义
- 树是n（n>=0）个节点的有限集，且这些节点满足如下关系：
- （1）有且仅有一个节点没有父节点，该节点称为树的根。
- （2）除根外，其余的每个节点都有且仅有一个父节点。
- （3）树中的每一个节点都构成一个以它为根的树。
+ 预备知识：二叉树的深度遍历
  
- 二叉树在满足树的条件时，满足如下条件：
- 每个节点最大有两个孩子（子树），这两个子树有左右之分，次序不可颠倒。
  */
+class PrePareBinaryTree2 {
+    void traversal(TreeNode *node) {
+        if (!node) {
+            return;
+        }
+        //此时访问node称为前序遍历
+        traversal(node->left);
+        //此时访问node称为中序遍历
+        //此时访问node称为后序遍历
+    }
+};
+
+
+#endif /* PrePareBinaryTree_hpp */
+
